f2bbeac3a00a550585f9f8ff467a3492bdf85ca1
rraangga <29746989+rraangga@users.noreply.github.com>
2021-02-22 01:42:40 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:80/wpu-login/';
+$config['base_url'] = 'https://rraangga.github.io/wpu-login/';
 
 /*
 |--------------------------------------------------------------------------
